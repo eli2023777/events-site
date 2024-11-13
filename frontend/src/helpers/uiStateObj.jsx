a01f4eef21952dce0650c3b805fb8e656fb14930
Eli Holzman <eli770440@gmail.com>
2024-11-13 14:38:48 +0200

    move out ui_state obj to helpers

---
+++ frontend/src/helpers/uiStateObj.jsx
@@ -0,0 +1,12 @@
+
+export const UI_STATE = {
+    CALENDAR: 'CALANDER',
+    EVENTS: 'EVENTS'
+};
+
+
+// export const EVENT_STATUS = {
+//     PENDING: 'PENDING',
+//     CONFIRMED: 'CONFIRMED',
+//     CANCELED: 'CANCELED'
+// };
