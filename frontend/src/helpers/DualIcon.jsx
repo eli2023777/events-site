a7be4015c419c0d582860b0850e8ed5eefafcb46
Eli Holzman <eli770440@gmail.com>
2024-11-15 12:24:54 +0200

    Use dualIcon for mor icons

--- frontend/src/helpers/DualIcon.jsx
+++ frontend/src/helpers/DualIcon.jsx
@@ -6,18 +6,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faUser as faUserSolid,
     faEdit as faEditSolid,
-    faTrash as faTrashSolid
+    faTrash as faTrashSolid,
+    faCalendarDays as faCalendarDaysSolid,
+    faList as faListSolid
 } from '@fortawesome/free-solid-svg-icons';
 
 // Regular icons
 import {
     faUser as faUserRegular,
     faEdit as faEditRegular,
-    faTrashAlt as faTrashRegular
+    faTrashAlt as faTrashRegular,
+    faCalendarDays as faCalendarDaysRegular,
+    faListAlt as faListRegular
 } from '@fortawesome/free-regular-svg-icons';
 
 // Add all icons to library
-library.add(faUserSolid, faEditSolid, faTrashSolid, faUserRegular, faEditRegular, faTrashRegular);
+library.add(faUserSolid, faEditSolid, faTrashSolid, faCalendarDaysSolid, faListSolid,
+    faUserRegular, faEditRegular, faTrashRegular, faCalendarDaysRegular, faListRegular);
 
 
 export const DualIcon = ({ iconName }) => {
@@ -27,7 +32,9 @@ export const DualIcon = ({ iconName }) => {
     const iconMap = {
         user: { solid: faUserSolid, regular: faUserRegular },
         edit: { solid: faEditSolid, regular: faEditRegular },
-        trash: { solid: faTrashSolid, regular: faTrashRegular }
+        trash: { solid: faTrashSolid, regular: faTrashRegular },
+        calendar: { solid: faCalendarDaysSolid, regular: faCalendarDaysRegular },
+        list: { solid: faListSolid, regular: faListRegular }
     };
 
     // Choose regular or solid
